26f23c39d835efe2917bc9b6365e25e78736f091
Sadigkhan Gaykhanov <90463823+Sadigkhan@users.noreply.github.com>
2022-02-23 11:59:14 +0400

    Add files via upload

---
+++ JS-2/js.js
@@ -0,0 +1,29 @@
+var btn = document.querySelector(".btn")
+var add = document.querySelector(".add")
+var list = document.querySelector(".list")
+var del=document.querySelector(".backspace")
+
+var body=document.body
+
+
+btn.addEventListener("click", function () {
+
+  var value = add.value
+  var li = document.createElement("li")
+  li.innerHTML = value
+  list.appendChild(li)
+})
+
+function BackSpace(){ 
+  var input=document.querySelector(".add").value;
+  var del=input.slice(0,-1);
+  document.querySelector(".add").value=del;
+}
+
+
+function Change(){ 
+  var value=document.querySelector(".changer").value;
+  body.style.backgroundColor=value
+  body.setAttribute('class',value)
+}
+
